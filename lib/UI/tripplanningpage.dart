13bbdc7b27bdb9b00e7ee2596b51f2dc0fcac86a
Kevin Oommen <kevinoommenanish@gmail.com>
2023-05-05 18:14:52 +0530

    Updated planning page

--- lib/UI/tripplanningpage.dart
+++ lib/UI/tripplanningpage.dart
@@ -1,93 +1,129 @@
 import 'package:flutter/material.dart';
 
 class TripPlanningPage extends StatefulWidget {
+  const TripPlanningPage({Key? key}) : super(key: key);
+
   @override
   _TripPlanningPageState createState() => _TripPlanningPageState();
 }
 
 class _TripPlanningPageState extends State<TripPlanningPage> {
-  final _destinationController = TextEditingController();
-  final _startDateController = TextEditingController();
-  final _endDateController = TextEditingController();
-
+  final TextEditingController _destinationController = TextEditingController();
+  final TextEditingController _noOfPersonsController = TextEditingController();
   DateTime _startDate = DateTime.now();
   DateTime _endDate = DateTime.now();
+
   Future<void> _selectStartDate(BuildContext context) async {
-    final picked = await showDatePicker(
+    final DateTime? picked = await showDatePicker(
       context: context,
-      initialDate: _startDate ?? DateTime.now(),
+      initialDate: _startDate,
       firstDate: DateTime.now(),
-      lastDate: DateTime(2100),
+      lastDate: DateTime(2101),
     );
-    if (picked != null && picked != _startDate) {
+    if (picked != null && picked != _startDate)
       setState(() {
         _startDate = picked;
-        _startDateController.text = picked.toString();
       });
-    }
   }
 
   Future<void> _selectEndDate(BuildContext context) async {
-    final picked = await showDatePicker(
+    final DateTime? picked = await showDatePicker(
       context: context,
-      initialDate: _endDate ?? DateTime.now().add(Duration(days: 1)),
-      firstDate: _startDate ?? DateTime.now().add(Duration(days: 1)),
-      lastDate: DateTime(2100),
+      initialDate: _endDate,
+      firstDate: DateTime.now(),
+      lastDate: DateTime(2101),
     );
-    if (picked != null && picked != _endDate) {
+    if (picked != null && picked != _endDate)
       setState(() {
         _endDate = picked;
-        _endDateController.text = picked.toString();
       });
-    }
-  }
-
-  @override
-  void dispose() {
-    _destinationController.dispose();
-    _startDateController.dispose();
-    _endDateController.dispose();
-    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Plan Your Trip'),
+        title: Text('Trip Planning'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            Text(
+              'Where do you want to go?',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
             TextField(
               controller: _destinationController,
               decoration: InputDecoration(
-                labelText: 'Destination',
+                hintText: 'Enter your destination',
                 border: OutlineInputBorder(),
               ),
             ),
-            SizedBox(height: 16.0),
+            SizedBox(height: 20),
+            Text(
+              'When do you want to go?',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
+            Row(
+              children: [
+                Expanded(
+                  child: InkWell(
+                    onTap: () => _selectStartDate(context),
+                    child: InputDecorator(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Start Date',
+                      ),
+                      child: Text(
+                        '${_startDate.toLocal()}'.split(' ')[0],
+                        style: TextStyle(fontSize: 16),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 10),
+                Expanded(
+                  child: InkWell(
+                    onTap: () => _selectEndDate(context),
+                    child: InputDecorator(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'End Date',
+                      ),
+                      child: Text(
+                        '${_endDate.toLocal()}'.split(' ')[0],
+                        style: TextStyle(fontSize: 16),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            Text(
+              'How many persons?',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
             TextField(
-              controller: _startDateController,
-              readOnly: true,
-              onTap: () => _selectStartDate(context),
+              controller: _noOfPersonsController,
+              keyboardType: TextInputType.number,
               decoration: InputDecoration(
-                labelText: 'Start Date',
+                hintText: 'Enter number of persons',
                 border: OutlineInputBorder(),
-                suffixIcon: Icon(Icons.calendar_today),
               ),
             ),
-            SizedBox(height: 16.0),
-            TextField(
-              controller: _endDateController,
-              readOnly: true,
-              onTap: () => _selectEndDate(context),
-              decoration: InputDecoration(
-                labelText: 'End Date',
-                border: OutlineInputBorder(),
-                suffixIcon: Icon(Icons.calendar_today),
+            SizedBox(height: 40),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {
+                  // code to book the trip
+                }, child: Text('Book Trip'),
               ),
             ),
           ],
@@ -96,3 +132,4 @@ class _TripPlanningPageState extends State<TripPlanningPage> {
     );
   }
 }
+

c2f984121b40abf01060653565c0a4e8c2eed477
GouthamCArun <gouthamchennamakkal@gmail.com>
2023-05-05 10:07:08 +0530

    kerala

--- lib/UI/kerala.dart
+++ lib/UI/kerala.dart
@@ -1,69 +1,226 @@
 import 'package:flutter/material.dart';
-import 'package:proton/UI/chatbot.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'chatbot.dart';
 
 class Kerala extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        top: true,
+      body: Padding(
+        padding: const EdgeInsets.all(25.0),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              padding: const EdgeInsets.only(top: 40.0),
               child: Text(
-                'My App',
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                'choose the best locations',
+                textAlign: TextAlign.left,
+                style: GoogleFonts.getFont(
+                  "Poppins",
+                  textStyle: const TextStyle(
+                      color: Color.fromARGB(255, 0, 0, 0),
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold),
+                ),
               ),
             ),
-            Expanded(
-              child: GestureDetector(
-                onTap: () {
-                  // Navigate to another page when the card is tapped
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) =>ConversationScreen(chatRoomId: 'xxx',userName: 'YourIndianFriend')),
-                  );
-                },
-                child: SizedBox(
-                  height: 10,
+            Row(
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(left: 30, right: 90),
+                  child: Text(
+                    textAlign: TextAlign.left,
+                    'Popular Places',
+                    style: GoogleFonts.getFont(
+                      "Poppins",
+                      textStyle: const TextStyle(
+                        color: Color.fromARGB(255, 0, 0, 0),
+                        fontSize: 15,
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  width: 100,
+                  height: 40,
+                  child: Card(
+                    color: Color.fromARGB(255, 0, 0, 0),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.only(left: 27, top: 9),
+                      child: Text(
+                        'View all',
+                        textAlign: TextAlign.left,
+                        style: GoogleFonts.getFont(
+                          "Poppins",
+                          textStyle: const TextStyle(
+                              color: Color.fromARGB(255, 255, 255, 255),
+                              fontSize: 10,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Container(
+                width: 325,
+                height: 200,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                  color: Colors.blue,
+                ),
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => ConversationScreen(
+                              chatRoomId: 'xxx', userName: 'YourIndianFriend')),
+                    );
+                  },
                   child: Card(
                     shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(50),
+                      borderRadius: BorderRadius.circular(20.0),
                     ),
-                    margin: const EdgeInsets.all(16),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        ClipRRect(
-                          borderRadius:
-                              BorderRadius.vertical(top: Radius.circular(10)),
-                          child: Image.asset(
-                            'assets/images/delhi.jpg',
-                            width: double.infinity,
-                            height: 200,
+                    child: Center(
+                      child: Container(
+                        width: 325,
+                        height: 200,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage('assets/images/kala.jpg'),
                             fit: BoxFit.cover,
                           ),
                         ),
-                        Padding(
-                          padding: const EdgeInsets.all(16),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                'Title',
-                                style: TextStyle(fontSize: 24),
+                        child: Padding(
+                          padding: EdgeInsets.only(left: 30, top: 140.0),
+                          child: Text(
+                            'Kerala Kalamandalam',
+                            textAlign: TextAlign.left,
+                            style: GoogleFonts.getFont(
+                              "Mitr",
+                              textStyle: const TextStyle(
+                                color: Color.fromARGB(255, 255, 255, 255),
+                                fontSize: 20,
                               ),
-                              SizedBox(height: 10),
-                              Text(
-                                'Description',
-                                style: TextStyle(fontSize: 18),
-                              ),
-                            ],
+                            ),
                           ),
                         ),
-                      ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Container(
+                width: 325,
+                height: 200,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                  color: Colors.blue,
+                ),
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => ConversationScreen(
+                              chatRoomId: 'xxx', userName: 'YourIndianFriend')),
+                    );
+                  },
+                  child: Card(
+                    // color: Color.fromARGB(255, 0, 0, 0),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
+                    child: Center(
+                      child: Container(
+                        width: 325,
+                        height: 200,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage('assets/images/kuttanad.jpg'),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        child: Padding(
+                          padding: EdgeInsets.only(left: 30, top: 140.0),
+                          child: Text(
+                            'Kuttanad',
+                            textAlign: TextAlign.left,
+                            style: GoogleFonts.getFont(
+                              "Mitr",
+                              textStyle: const TextStyle(
+                                color: Color.fromARGB(255, 255, 255, 255),
+                                fontSize: 20,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Container(
+                width: 325,
+                height: 200,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                  color: Colors.blue,
+                ),
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => ConversationScreen(
+                              chatRoomId: 'xxx', userName: 'YourIndianFriend')),
+                    );
+                  },
+                  child: Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
+                    child: Center(
+                      child: Container(
+                        width: 325,
+                        height: 200,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage(
+                                'assets/images/places-to-visit-in-fort-kochi.jpg'),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        child: Padding(
+                          padding: EdgeInsets.only(left: 30, top: 140.0),
+                          child: Text(
+                            'Fort Kochi',
+                            textAlign: TextAlign.left,
+                            style: GoogleFonts.getFont(
+                              "Mitr",
+                              textStyle: const TextStyle(
+                                color: Color.fromARGB(255, 255, 255, 255),
+                                fontSize: 20,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
                     ),
                   ),
                 ),
@@ -76,14 +233,43 @@ class Kerala extends StatelessWidget {
   }
 }
 
-class MyOtherPage extends StatelessWidget {
+class DetailPage extends StatelessWidget {
+  final String title;
+  final String imagePath;
+
+  DetailPage(this.title, this.imagePath);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: Text('This is another page!'),
-        ),
+      body: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            height: 200,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(imagePath),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Center(
+              child: Text(
+                title,
+                style: TextStyle(fontSize: 24, color: Colors.white),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(16),
+              child: Text(
+                'This is the detail page for $title.',
+                style: TextStyle(fontSize: 18),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
